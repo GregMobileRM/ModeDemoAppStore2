8a9654d526e84d3f4763abd2625ea51c7112cfd7
GregMobileRM <gregory.s.schuessler@monsanto.com>
2017-03-08 14:55:21 -0600

    removed push notifications

---
+++ Carthage/Checkouts/QorumLogs/QorumLogsExample/QorumLogsExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  QorumLogsExample
+//
+//  Created by Goktug Yilmaz on 4/6/16.
+//  Copyright © 2016 Goktug Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
